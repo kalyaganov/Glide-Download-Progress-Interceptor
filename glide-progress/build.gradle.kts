fc100e648ee5751b9978ff978941b7597d444d3e
Alexey Kalyaganov <aa.kalyaganov@yandex.ru>
2026-07-06 00:01:57 +0300

    Publish to Maven Central (#2)
    * Add Maven Central publishing config (maven-publish + signing)
    
    * Add Maven Central publishing via vanniktech plugin
    
    * Mark 2.0.0 release, add namespace + CI changes to CHANGELOG

--- glide-progress/build.gradle.kts
+++ glide-progress/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.29.0"
+    id("signing")
 }
 
 android {
@@ -50,4 +52,39 @@ dependencies {
     androidTestImplementation(libs.androidx.test.core)
     androidTestImplementation(libs.androidx.test.runner)
     androidTestImplementation(libs.androidx.test.rules)
+}
+
+mavenPublishing {
+    publishToMavenCentral("CENTRAL_PORTAL")
+
+    coordinates("ru.kalyaganov", "glide-download-interceptor", "2.0.0")
+
+    pom {
+        name.set("Glide Download Progress Interceptor")
+        description.set("OkHttp network interceptor for tracking Glide image download progress on Android")
+        url.set("https://github.com/kalyaganov/Glide-Download-Progress-Interceptor")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("kalyaganov")
+                name.set("Alexey")
+                email.set("alexey@kalyaganov.ru")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/kalyaganov/Glide-Download-Progress-Interceptor.git")
+            developerConnection.set("scm:git:ssh://github.com/kalyaganov/Glide-Download-Progress-Interceptor.git")
+            url.set("https://github.com/kalyaganov/Glide-Download-Progress-Interceptor")
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
 }
